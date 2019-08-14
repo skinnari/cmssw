c73e227a1f1381e8ed03a21a77976de1f6cdb4cb
Gianluca <gianluca.cerminara@cern.ch>
2019-08-14 17:34:29 +0200

    read the calibrations from cfg

--- L1Trigger/L1THGCal/plugins/backend/HGCalTriggerClusterInterpretationEM.cc
+++ L1Trigger/L1THGCal/plugins/backend/HGCalTriggerClusterInterpretationEM.cc
@@ -3,6 +3,7 @@
 // #include "FWCore/ParameterSet/interface/FileInPath.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "L1Trigger/L1THGCal/interface/backend/HGCalTriggerClusterInterpreterBase.h"
+#include "DataFormats/L1THGCal/interface/HGCalMulticluster.h"
 // #include "L1Trigger/L1THGCal/interface/HGCalTriggerTools.h"
 // #include "CommonTools/MVAUtils/interface/TMVAEvaluator.h"
 
@@ -18,7 +19,8 @@ public:
                          l1t::HGCalMulticlusterBxCollection& multiclusters) const final;
 
 private:
-
+  std::vector<double> layer_containment_corrs_;
+  std::vector<double> scale_corrections_coeff_;
 };
 
 DEFINE_HGC_TPG_CLUSTER_INTERPRETER(HGCalTriggerClusterInterpretationEM, "HGCalTriggerClusterInterpretationEM");
@@ -26,11 +28,20 @@ DEFINE_HGC_TPG_CLUSTER_INTERPRETER(HGCalTriggerClusterInterpretationEM, "HGCalTr
 
 HGCalTriggerClusterInterpretationEM::HGCalTriggerClusterInterpretationEM() {}
 
-void HGCalTriggerClusterInterpretationEM::initialize(const edm::ParameterSet& conf) {}
+void HGCalTriggerClusterInterpretationEM::initialize(const edm::ParameterSet& conf) {
+  layer_containment_corrs_ = conf.getParameter<std::vector<double>>("layer_containment_corr");
+  scale_corrections_coeff_ = conf.getParameter<std::vector<double>>("scale_correction_coeff");
+}
+
 void HGCalTriggerClusterInterpretationEM::interpret(// const std::vector<std::pair<GlobalPoint, double>>& seedPositionsEnergy,
                                                     // const HGCalTriggerGeometryBase& triggerGeometry,
                                                     l1t::HGCalMulticlusterBxCollection& multiclusters) const {
                                                       for(auto cluster3d: multiclusters) {
-                                                        std::cout << cluster3d.detId() << " e (GeV): " << cluster3d.energy() << std::endl;
+                                                        double energy = cluster3d.energy();
+                                                        cluster3d.saveEnergyInterpretation(l1t::HGCalMulticluster::EnergyInterpretation::EM, energy);
+                                                        std::cout << cluster3d.detId()
+                                                                  << " E (GeV): " << cluster3d.energy()
+                                                                  << " E_EM (GeV): " << cluster3d.iEnergy(l1t::HGCalMulticluster::EnergyInterpretation::EM) << std::endl;
+
                                                       }
                                                     }
