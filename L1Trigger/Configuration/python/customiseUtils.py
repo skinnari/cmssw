cd367e46d2eaee058bf7045a91ff2d64b4b92add
Louise Skinnari <louise.skinnari@cern.ch>
2019-10-15 19:26:37 +0200

    minor edits to configuration

--- L1Trigger/Configuration/python/customiseUtils.py
+++ L1Trigger/Configuration/python/customiseUtils.py
@@ -203,14 +203,14 @@ def L1TrackTriggerTracklet(process):
 
 def L1TrackTriggerHybrid(process):
     #print "L1T INFO:  run the L1TrackStep with Tracklet."
-    process.load('L1Trigger.TrackFindingTracklet.Tracklet_cfi')
+    process.load('L1Trigger.TrackFindingTracklet.L1TrackletEmulationTracks_cff')
     process.L1TrackTriggerHybrid_step = cms.Path(process.TTTracksFromTrackletEmulation)
     process.schedule.insert(2,process.L1TrackTriggerHybrid_step)
     return process
 
 def L1TrackTriggerDisplacedHybrid(process):
     #print "L1T INFO:  run the L1TrackStep with Tracklet."
-    process.load('L1Trigger.TrackFindingTracklet.Tracklet_cfi')
+    process.load('L1Trigger.TrackFindingTracklet.L1ExtendedTrackletEmulationTracks_cff')
     process.L1TrackTriggerDisplacedHybrid_step = cms.Path(process.TTTracksFromExtendedTrackletEmulation)
     process.schedule.insert(2,process.L1TrackTriggerDisplacedHybrid_step)
     return process
--- L1Trigger/TrackFindingTracklet/test/Tracklet_cfg.py
+++ L1Trigger/TrackFindingTracklet/test/Tracklet_cfg.py
@@ -9,10 +9,10 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
-#process.load('Configuration.Geometry.GeometryExtended2023D21Reco_cff') ## this needs to match the geometry you are running on
-#process.load('Configuration.Geometry.GeometryExtended2023D21_cff')     ## this needs to match the geometry you are running on
-process.load('Configuration.Geometry.GeometryExtended2023D41Reco_cff') ## this needs to match the geometry you are running on
-process.load('Configuration.Geometry.GeometryExtended2023D41_cff')     ## this needs to match the geometry you are running on
+process.load('Configuration.Geometry.GeometryExtended2023D35Reco_cff') ## this needs to match the geometry used
+process.load('Configuration.Geometry.GeometryExtended2023D35_cff')     ## this needs to match the geometry used
+#process.load('Configuration.Geometry.GeometryExtended2023D41Reco_cff') ## this needs to match the geometry used (D41: change in ../interface/Constants.h to geomTkTDR=false)
+#process.load('Configuration.Geometry.GeometryExtended2023D41_cff')     ## this needs to match the geometry used (D41: change in ../interface/Constants.h to geomTkTDR=false)
 
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
@@ -22,11 +22,11 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS3', '')
 
 
 # input
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5))
 Source_Files = cms.untracked.vstring(
-#"/store/relval/CMSSW_10_4_0/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/PU25ns_103X_upgrade2023_realistic_v2_2023D21PU200-v1/20000/FFF48AB4-E5E6-3842-8A5B-20E2B7E497BC.root"
-"/store/relval/CMSSW_10_6_0_pre4/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/PU25ns_106X_upgrade2023_realistic_v2_2023D41PU200-v1/10000/FEA5D564-937A-8D4B-9C9A-696EFC05AB58.root"
-)
+    "/store/mc/PhaseIIMTDTDRAutumn18DR/TTbar_14TeV_TuneCP5_Pythia8/FEVT/PU200_103X_upgrade2023_realistic_v2-v1/80003/FFF35381-A845-4841-9364-923285FFCFA6.root"
+#    "/store/mc/PhaseIITDRSpring19DR/TTbar_14TeV_TuneCP5_Pythia8/GEN-SIM-DIGI-RAW/PU200_106X_upgrade2023_realistic_v3_ext1-v3/60000/FFB5D0CA-208F-6040-A9BF-3F5354D0AA59.root"
+    )
 process.source = cms.Source("PoolSource", fileNames = Source_Files)
 
 
@@ -35,11 +35,21 @@ process.source = cms.Source("PoolSource", fileNames = Source_Files)
 #process.TTTracks = cms.Path(process.L1TrackletTracks)                         #run only the tracking (no MC truth associators)
 #process.TTTracksWithTruth = cms.Path(process.L1TrackletTracksWithAssociators) #run the tracking AND MC truth associators)
 
-# L1 tracking => emulation 
+# L1 tracking => hybrid emulation 
+#process.load("L1Trigger.TrackFindingTracklet.L1TrackletEmulationTracks_cff")
+#process.TTTracksEmulation = cms.Path(process.L1TrackletEmulationTracks)
+#process.TTTracksEmulationWithTruth = cms.Path(process.L1TrackletEmulationTracksWithAssociators)
+
+# L1 tracking => extended hybrid emulation (displaced)                  
+#process.load("L1Trigger.TrackFindingTracklet.L1ExtendedTrackletEmulationTracks_cff")      
+#process.TTTracksExtendedEmulation = cms.Path(process.L1ExtendedTrackletEmulationTracks)
+#process.TTTracksExtendedEmulationWithTruth = cms.Path(process.L1ExtendedTrackletEmulationTracksWithAssociators)  
+
+# L1 tracking => floating-point version + hybrid emulation
+process.load("L1Trigger.TrackFindingTracklet.L1TrackletTracks_cff")
+process.TTTracksWithTruth = cms.Path(process.L1TrackletTracksWithAssociators)
 process.load("L1Trigger.TrackFindingTracklet.L1TrackletEmulationTracks_cff")
 process.TTTracksEmulation = cms.Path(process.L1TrackletEmulationTracks)
-process.TTTracksEmulationWithTruth = cms.Path(process.L1TrackletEmulationTracksWithAssociators)
-
 
 # output module
 process.out = cms.OutputModule( "PoolOutputModule",
@@ -53,6 +63,5 @@ process.out = cms.OutputModule( "PoolOutputModule",
 )
 process.FEVToutput_step = cms.EndPath(process.out)
 
-#process.schedule = cms.Schedule(process.TTTracksWithTruth,process.FEVToutput_step)
-process.schedule = cms.Schedule(process.TTTracksEmulationWithTruth,process.FEVToutput_step)
+process.schedule = cms.Schedule(process.TTTracksWithTruth,process.TTTracksEmulation,process.FEVToutput_step)
 
